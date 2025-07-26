e144f6ee154511b2adc60838020727d8fac961ff
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2025-07-26 14:34:38 -0500

    Making static pages available

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/venkata_aavula",
+  assetPrefix: "/venkata_aavula/",
 };
 
 export default nextConfig;
