145369f76f41224dc600b5fd53637877a7ba0a53
Venkata Sai Aavula <venkatasaiaavula@gmail.com>
2025-07-26 15:00:25 -0500

    delete unnecessary files for gh-pages

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/venkata_aavula",
-  assetPrefix: "/venkata_aavula/",
-};
-
-export default nextConfig;
